80233149a089b6d025eb93cf2b6c9d7fde4131d7
marcleroy <marc.ml.1501@gmail.com>
2024-02-23 16:11:36 +0100

    Add project files.

---
+++ CoffeeBrowser.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeBrowser.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
